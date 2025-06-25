8ada0bf643dc16be68b236acb15d6dddca30ba33
Matheus Alves <162912512+Matheu55@users.noreply.github.com>
2025-06-25 11:35:21 -0300

    Add files via upload

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,34 +1,35 @@
-import './Rodape.css'
+
+import './rodape.css'
 
 const Rodape = () => {
     return (<footer className="footer">
-        <section>
-            <ul>
-                <li>
-                    <a href="facebook.com" target="_blank">
-                        <img src="/imagens/facebook.png" alt="" />
-                    </a>
-                </li>
-                <li>
-                    <a href="twitter.com" target="_blank">
-                        <img src="/imagens/twitter.png" alt="" />
-                    </a>
-                </li>
-                <li>
-                    <a href="instagram.com" target="_blank">
-                        <img src="/imagens/instagram.png" alt="" />
-                    </a>
-                </li>
-            </ul>
-        </section>
-        <section>
-            <img src="/imagens/logo.png" alt="" />
-        </section>
-        <section>
-            <p>
-                Desenvolvido por Matheus com apoio da Alura.
-            </p>
-        </section>
+       <section>
+       <ul>
+            <li>
+                <a href="facebook.com" target="_blank">
+                    <img src="/imagens/facebook.png" alt="Icone facebook" />
+                </a>
+            </li>
+            <li>
+                <a href="twitter.com" target="_blank">
+                    <img src="/imagens/twitter.png" alt="Icone twiteer" />
+                </a>
+            </li>
+            <li>
+                <a href="instagram.com" target="_blank">
+                    <img src="/imagens/instagram.png" alt="Icone instagram" />
+                </a>
+            </li>
+        </ul>
+       </section>
+       <section>
+       <img src="/imagens/logo.png" alt="" />
+       </section>
+       <section>
+        <p>
+            Desenvolvido por Matheus com ajuda da Alura.
+        </p>
+       </section>
     </footer>)
 }
 
